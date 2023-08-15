d1e9cd727e54a252eab72715206f52d36324c547
Kate <katepolishchuk13@gmail.com>
2023-08-15 12:33:05 -0400

    finished day_9

--- day_9/day_9_#2.py
+++ day_9/day_9_#2.py
@@ -1,106 +1,21 @@
-def read_lines():
-    with open("day_9.txt") as f:
-        return f.read().splitlines()
-    
-
-class Vector: 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        
-    def __add__(self, other):
-        return Vector(self.x + other.x, 
-                      self.y + other.y)
-        
-    def __sub__(self, other):
-        return Vector(self.x - other.x, 
-                      self.y - other.y)
-
-    def __repr__(self) -> str:
-        return f"Vector({self.x}, {self.y})" 
-    
-    def __eq__(self, other) -> bool:
-        return self.x == other.x and self.y == other.y
-    
-    def __hash__(self):
-        return hash((self.x, self.y))
-    
-    @property
-    def max_norm(self):
-        return max(abs(self.x), abs(self.y))  
-    
-    @property
-    def l2_norm_squared(self):
-        return self.x**2 + self.y**2 
-    
-DIRECTIONS = {
-        'R': Vector(1, 0),
-        'L': Vector(-1, 0),
-        'U': Vector(0, 1),
-        'D': Vector(0, -1),
-    }
-
-
-def get_head_moves(lines):
-    moves: list[Vector] = []
-    for line in lines:
-        direction, amount_str = line.split(" ")
-        move: Vector = DIRECTIONS[direction]
-        for _ in range(int(amount_str)):
-            moves.append(move)
-    return moves
-
-possible_tail_steps = [Vector(x, y) 
-                       for x in [-1, 0, 1]
-                       for y in [-1, 0, 1]
-                       if (x, y) != (0, 0)]
-
-def chase(head: Vector, tail: Vector):
-    """
-    .....
-    .....
-    ..T..
-    .....
-    .....
-    """
-    
-    difference = head - tail
-    if difference.max_norm <= 1:
-        return tail
-    best_step = get_best_step(goal_position = difference)
-    return tail + best_step
-
-def get_best_step(goal_position: Vector) -> Vector:
-    best_step = Vector(0, 0)
-    best_l2_norm_sq = 100
-    for step in possible_tail_steps:
-        l2_norm_sq = (goal_position - step).l2_norm_squared
-        if l2_norm_sq < best_l2_norm_sq:
-            best_step = step
-            best_l2_norm_sq = l2_norm_sq
-    return best_step
+from part1 import Vector, read_lines, get_head_moves, chase 
 
 def solve(lines):
-    head_position = Vector(0, 0)
-    tail_position = Vector(0, 0)
-    tail_positions: set[Vector] = {tail_position}
+    rope = [Vector(0, 0) for _ in range(10)]
+    tail_positions: set[Vector] = {rope[9]}
     head_moves = get_head_moves(lines)
     for head_move in head_moves:
-        head_position += head_move
-        tail_position = chase(head_position, tail_position)
-        tail_positions.add(tail_position)
+        rope[0] += head_move
+        for i in range(1, 10):
+            rope[i] = chase(rope[i-1], rope[i]) 
+        tail_positions.add(rope[9])
         
     return len(tail_positions)
 
 def main():
-    lines = read_lines() 
-    solution = solve(lines)
-    print(f'{solution = }')
-    
-    
+    lines = read_lines()
+    solution = solve(lines) 
+    print(f"{solution = }")
+
 if __name__ == '__main__':
     main()
-    
-
-
-        
